d487d5adb6d5b6de574c888f18810b3d10d7fa69
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2024-09-03 17:17:41 -0300

    feat: Enable CORS for all origins and specific methods and headers

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
+  app.enableCors({
+    origin: '*', // Permitir qualquer origem. Você pode substituir por um domínio específico.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+  
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
